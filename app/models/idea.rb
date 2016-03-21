c9e7c3760c0c4ab05bdb5011d608fa9d0a5ef740
James Crockett <jecrockett@gmail.com>
2016-03-21 15:45:31 -0600

    Add validations and default scope to idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,8 @@
 class Idea < ActiveRecord::Base
+  validates :title, presence: true
+  validates :body,  presence: true
+
+  before_create :set_quality
+
+  default_scope { order(:created_at) }
 end
